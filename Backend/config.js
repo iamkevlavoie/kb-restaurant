cb4ec8ab0167ee62bd23ff94005308ba64fc0d18
Kevin Lavoie <98438714+iamkevlavoie@users.noreply.github.com>
2022-07-07 18:46:28 -0600

    Delete config.js

--- Backend/config.js
+++
@@ -1,6 +0,0 @@
-const Sequelize = require('sequelize');
-const config = new Sequelize("kbrestaurant", "bobby", "5AR)^Ro@<hi7C1Pf", {
-    host: '34.83.241.31',
-    dialect: 'mysql'});
-
-module.exports = config;
